a785777a4b7a034ed675e990fd54b20bf230a59e
davi38 <davi4alves@gmail.com>
2022-07-25 22:48:33 -0300

    JS começado

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import './App.css';
+import {incrementarCasa1,
+        incrementarCasa2,
+        incrementarCasa3,
+        incrementarVisitante1,
+        incrementarVisitante2,
+        incrementarVisitante3} from './button.js'
+
 
 function App() {
   return (
@@ -6,20 +13,20 @@ function App() {
     <div className="fundo-placar">
        <div className='placar-visitante'>
         <div className="numero-visitante">
-          <h1 id='placar-casa'>0</h1>
+          <h1 id='placar-vsitante'>0</h1>
         </div>
         <div className='adicionar'>
-        <button id='visitante-botao-1'>+ 1</button>
+        <button id='visitante-botao-1' >+ 1</button>
         <button id='visitante-botao-2'>+ 2</button>
         <button id='visitante-botao-3'>+ 3</button>
         </div>
          </div>
        <div className='placar-casa'>
         <div className="numero-casa">
-          <h1 id='placar-casa'>1</h1>
+          <h1 id='placar-casa'>0</h1>
         </div>
         <div className='adicionar'>
-        <button id='casa-botao-1'>+ 1</button>
+        <button id='casa-botao-1' onClick={incrementarCasa1}>+ 1</button>
         <button id='casa-botao-2'>+ 2</button>
         <button id='casa-botao-3'>+ 3</button>
         </div>
--- src/button.js
+++ src/button.js
@@ -1,6 +1,20 @@
-let addUm = document.getElementById('')
+// placar <h1>
+var placarVisitante = document.getElementById('placar-visitante')
+var placarCasa = document.getElementById('placar-casa')
+var resultadoCasa = 0;
+var resultadoVisitante = 0;
 
 
-function incrementar(){
 
+export function incrementarCasa1(){
+    let result = resultadoCasa + 1
+    placarCasa.innerHTML = result;
+    document.getElementById('placar-casa').value = 10;
+    console.log(result);
+ return
 }
+export function incrementarCasa2(){}
+export function incrementarCasa3(){}
+export function incrementarVisitante1(){}
+export function incrementarVisitante2(){}
+export function incrementarVisitante3(){}
